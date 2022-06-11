f11a828f56b75ce747820818e54bbca0fd4d9934
niles <egbaidomehjamez@gmail.com>
2022-06-11 12:08:24 -0700

    credit page component

---
+++ src/components/credit/Index.vue
@@ -0,0 +1,19 @@
+<template>
+<Intro />
+</template>
+
+<script>
+import Intro from './Intro.vue'
+export default {
+    components: {
+        Intro
+    }
+}
+</script>
+
+<style scoped>
+div {
+height: 80%;
+    background-color: black;
+}
+</style>
---
+++ src/components/credit/Intro.vue
@@ -0,0 +1,166 @@
+<template>
+<section class="intro">
+    <div class="intro--box">
+        <div class="intro--credit--container">
+            <div class="intro--credit">
+                <h4>WHO MADE THIS SITE?</h4>
+                <ul>
+                    <li>
+                        <h5>
+                            <span>Olacodes</span> - UI Design and Art Direction
+                            </h5>
+                    </li>
+                    <li>
+                        <h5>
+                            <span>Niles</span> - Development and Art Direction
+                            </h5>
+                    </li>
+                    <li>
+                        <h5>
+                             <span>Olacodes</span> - Motion & Interaction Design
+                        </h5>
+                    </li>
+                </ul>
+            </div>
+            <div class="intro--credit">
+                <h4>TYPESETTING</h4>
+                <h5>
+                    Type for this website was set in Commisioner and Cabinet Grotesk
+                </h5>
+            </div>
+             <div class="intro--credit">
+                <h3>Our redesign of <span>Jemima Osunde’s</span> portfolio. </h3>
+                <h5>
+                    © 2022
+                </h5>
+            </div>
+            <div class="intro--credit">
+                <h4>Assets credits:</h4>
+                <h5>
+                    Lionheart, Shuga, Rumor has it, Quam’s money, Jemima Osunde
+                </h5>
+            </div>
+        </div>
+        <div class="dmca">
+            <div class="dmca--text">
+                  <h3>Request for content removal</h3>
+                  <div class="line"></div>
+              </div>
+        </div>
+        <div class="link">
+            <div class="link--rounded">
+                  <h5>
+                      <router-link to="/">Credits</router-link>
+                      </h5>
+              </div>
+        </div>
+    </div>
+</section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/mixin';
+@import '@/styles/typography';
+.intro {
+    padding: 4rem 7%;
+}
+.intro--credit--container {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: auto;
+    width: 80%;
+}
+.intro--credit {
+    margin-top: 1.3rem;
+        width: 419px;
+    h4 {
+        margin-bottom: 2rem;
+        font-weight: 700;
+        @include credits {
+            color: #000000;
+        }
+    }
+    ul {
+        li {
+            margin-top: 1.3rem;
+            h5 {
+        @include credits2 {
+            font-family: 'Commissioner', sans-serif;
+            color: #000000;
+        }
+    }
+        }
+    }
+    h5 {
+        @include credits2 {
+            font-family: 'Commissioner', sans-serif;
+            color: #000000;
+        }
+    }
+    h3 {
+        margin-bottom: 2rem;
+        @include credits2 {
+            font-family: 'Commissioner', sans-serif;
+            color: #000000;
+        }
+        span {
+            font-weight: bold;
+        }
+    }
+}
+.dmca {
+    margin-top: 1.5rem;
+     .dmca--text {
+        transition: all 0.6 ease-in-out;
+        pointer-events: none;
+        &:hover {
+                .line {
+                    width: 50px;
+                }
+            }
+        h3 {
+             @include socials {
+                color: #000000
+            }
+        }
+        .line {
+                width: 44px;
+                border: 0.8px solid #000000;
+                transition: all 0.6 ease-in-out;
+            }
+    }
+}
+.link {
+    @include flex(flex-end,);
+    margin-top: -0.4rem;
+    .link--rounded {
+        width: 112px;
+        height: 112px;
+        border: 1.5px solid #000000;
+        border-radius: 100px;
+        @include flex(center, center);
+        margin-right: 2rem;
+        transition: all 0.6s ease-in-out;
+        h5 {
+            @include socials {
+                color: #000000
+            }
+        }
+        &:hover {
+            border: 1.5px solid #ffffff;
+            background-color: #2c2c2c;
+            h5 {
+            @include socials {
+                color: #ffffff
+            }
+        }
+        }
+    }
+}
+</style>
