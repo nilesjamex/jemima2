b5052e96dcd1d01a72d1c0fd47150bda0b099954
niles <egbaidomehjamez@gmail.com>
2022-06-11 09:05:49 -0700

    new page

---
+++ src/views/CreditView.vue
@@ -0,0 +1,15 @@
+<template>
+  <Credit />
+</template>
+
+<script>
+// @ is an alias to /src
+import Credit from '@/components/credit/Index.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Credit
+  }
+}
+</script>
