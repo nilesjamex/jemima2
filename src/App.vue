8fd248f37fbd09805daf01c903a6b2ae358481db
niles <egbaidomehjamez@gmail.com>
2022-06-11 12:05:43 -0700

    added router animation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
 <Nav />
-  <router-view/>
+<div>
+  <router-view v-slot="{ Component }">
+    <transition name="route">
+      <component :is="Component" />
+    </transition>
+ </router-view> 
+</div>
 </template>
 <script>
 import Nav from '@/components/Nav.vue'
@@ -11,3 +17,18 @@ export default {
 }
 </script>
 
+<style scoped>
+.route-enter-from {
+  opacity: 0;
+}
+.route-enter-active {
+  opacity: 0;
+}
+.route-leave-to {
+  opacity: 0;
+  transition: all 0.7s ease-in-out;
+}
+.route-leave-active {
+  transition: all 0.7s ease-in-out;
+}  
+</style>
