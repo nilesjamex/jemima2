e8e95408c9ed5296075061b237575f2bd04f9b28
niles <egbaidomehjamez@gmail.com>
2022-06-11 09:05:05 -0700

    new routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CreditView from '../views/CreditView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/credit',
+    name: 'credit',
+    component: CreditView
   }
 ]
 
