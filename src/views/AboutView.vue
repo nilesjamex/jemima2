73c1e7fb53ff664581a908655032aee6d728eb9d
niles <egbaidomehjamez@gmail.com>
2022-06-15 08:40:44 -0700

    new oages

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <About />
+</template>
+
+<script>
+import About from '@/components/about/Index.vue'
+export default {
+    components: {
+        About
+    }
+}
+</script>
+
+<style>
+
+</style>
