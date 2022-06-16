6d7385af453671f52b1fd67a7215c1c431cb4d3c
niles <egbaidomehjamez@gmail.com>
2022-06-16 02:43:45 -0700

    made images slide on scroll

--- src/components/home/ImgSlider.vue
+++ src/components/home/ImgSlider.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="slider">
     <div class="container">
-      <div class="main--sliderContainer">
-        <div class="slider--container">
+      <div class="main--sliderContainer1">
+        <div class="slider--container1">
           <div class="test">
             <img src="@/assets/movielist/lionheart.svg" alt="arrow" />
           </div>
@@ -18,7 +18,7 @@
         </div>
       </div>
       <div class="main--sliderContainer2">
-        <div class="slider--container">
+        <div class="slider--container2">
           <div class="test">
             <img src="@/assets/movielist/rhi.svg" alt="arrow" />
           </div>
@@ -38,25 +38,55 @@
 </template>
 
 <script>
-export default {};
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
+export default {
+  mounted() {
+  gsap.timeline({ scrollTrigger: {
+        trigger: '.main--sliderContainer1',
+        x: 200,
+        end: '+=900',
+        scrub: 'true'
+      }
+    })
+    .to('.slider--container1', {
+      x: -480,
+      delay: 1
+    })
+
+    gsap.timeline({ scrollTrigger: {
+        trigger: '.main--sliderContainer2',
+        x: 200,
+        end: '+=900',
+        scrub: 'true'
+      }
+    })
+    .to('.slider--container2', {
+      x: 480,
+      delay: 1
+    })
+   
+  }
+};
 </script>
 
 <style scoped lang="scss">
 @import "@/styles/mixin";
 @import "@/styles/typography";
 
-.main--sliderContainer {
+.main--sliderContainer1 {
   display: flex;
   flex-wrap: nowrap;
   gap: 1rem;
   overflow: hidden;
   flex-shrink: 0;
 
-  .slider--container {
+  .slider--container1 {
     flex-shrink: 0;
     gap: 1.2rem;
-    animation: infinityScroll 20s linear infinite;
-    transition: animation 4s ease-in-out;
+    // animation: infinityScroll 20s linear infinite;
+    // transition: animation 4s ease-in-out;
     @include flex();
   }
 
@@ -73,15 +103,15 @@ export default {};
   }
 }
 
-@keyframes infinityScroll {
-  0% {
-    transform: translate3d(0, 0, 0);
-  }
+// @keyframes infinityScroll {
+//   0% {
+//     transform: translate3d(0, 0, 0);
+//   }
 
-  100% {
-    transform: translate3d(-30%, 0, 0);
-  }
-}
+//   100% {
+//     transform: translate3d(-30%, 0, 0);
+//   }
+// }
 .main--sliderContainer2 {
   display: flex;
   flex-wrap: nowrap;
@@ -89,11 +119,11 @@ export default {};
   overflow: hidden;
   flex-shrink: 0;
   margin-top: 2.5rem;
-  .slider--container {
+  .slider--container2 {
     flex-shrink: 0;
     gap: 1.2rem;
-    animation: infinityScroll2 20s linear infinite;
-    transition: animation 4s ease-in-out;
+    // animation: infinityScroll2 20s linear infinite;
+    // transition: animation 4s ease-in-out;
     @include flex(flex-end);
   }
 
@@ -110,13 +140,13 @@ export default {};
   }
 }
 
-@keyframes infinityScroll2 {
-  0% {
-    transform: translate3d(0, 0, 0);
-  }
+// @keyframes infinityScroll2 {
+//   0% {
+//     transform: translate3d(0, 0, 0);
+//   }
 
-  100% {
-    transform: translate3d(30%, 0, 0);
-  }
-}
+//   100% {
+//     transform: translate3d(30%, 0, 0);
+//   }
+// }
 </style>
